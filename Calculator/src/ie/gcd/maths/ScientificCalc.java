d06aee9db87f27eb2c252fc313f4521351cd3af7
MairinK <mairinkavanagh@gmail.com>
2013-04-11 11:57:43 +0100

    merging

---
+++ Calculator/src/ie/gcd/maths/ScientificCalc.java
@@ -0,0 +1,17 @@
+package ie.gcd.maths;
+
+public class ScientificCalc {
+
+	public ScientificCalc() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
